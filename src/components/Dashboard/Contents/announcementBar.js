6dd4c0ce9dbd3678d3932c2147d87e2895d52828
Zadrick <zadrick.d.geron@accenture.com>
2022-06-01 22:53:08 +0800

    Announcement bar and initial appbar

--- src/components/Dashboard/Contents/announcementBar.js
+++ src/components/Dashboard/Contents/announcementBar.js
@@ -1,17 +1,25 @@
 import React, { Component } from "react";
-import Marquee from "react-fast-marquee";
-import { DialogContent, Grid, Typography } from "@mui/material";
+// import Marquee from "react-fast-marquee";
+import {
+  DialogTitle,
+  DialogContent,
+  Grid,
+  Typography,
+  Dialog,
+} from "@mui/material";
 import "./announcement.css";
-// const CustomFunc = require("../utils/customFunction");
+import CloseIcon from "@mui/icons-material/Close";
 
 class AnnouncementBar extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      //Dialog
+      isOpen: false,
       activeAnnouncements: [],
       mockAnnouncements: [
         {
-          text: "PromotionFree Shipping for FLX Members! Learn More",
+          text: "Free Shipping for FLX Members! Learn More",
         },
       ],
       isAnnounceVisible: "",
@@ -19,7 +27,10 @@ class AnnouncementBar extends Component {
   }
 
   componentDidMount() {}
-
+  //=============================UI FUNCTIONALITIES===============================//
+  handleClose() {
+    this.setState({ isOpen: false });
+  }
   onHideAnnounce = () => {
     sessionStorage.setItem("closedAnnounce", "none");
     this.setState({
@@ -37,12 +48,32 @@ class AnnouncementBar extends Component {
           }}
         >
           <Grid item xs={12} style={{ alignSelf: "center" }}>
-            {this.state.mockAnnouncements.length === 1 ? (
+            <div style={{ textAlign: "center" }}>
+              <Typography>
+                <span
+                  className="announcementText"
+                  onClick={() => {
+                    this.setState({ isOpen: true });
+                  }}
+                >
+                  Free Shipping for FLX Members! Learn More
+                </span>
+              </Typography>
+              &emsp;
+            </div>
+            {/* {this.state.mockAnnouncements.length === 1 ? (
               this.state.mockAnnouncements.map((x, i) => {
                 return (
                   <div style={{ textAlign: "center" }}>
                     <Typography>
-                      <span className="announcementText">{x.text}</span>
+                      <span
+                        className="announcementText"
+                        onClick={() => {
+                          this.setState({ isOpen: true });
+                        }}
+                      >
+                        {x.text}
+                      </span>
                     </Typography>
                     &emsp;
                   </div>
@@ -54,7 +85,12 @@ class AnnouncementBar extends Component {
                   return (
                     <div style={{ display: "flex" }}>
                       <Typography className="announcementText">
-                        <span style={{ fontWeight: 500, color: "#036ad8" }}>
+                        <span
+                          style={{ fontWeight: 500, color: "#036ad8" }}
+                          onClick={() => {
+                            this.setState({ isOpen: true });
+                          }}
+                        >
                           {x.text} &emsp;
                         </span>{" "}
                       </Typography>
@@ -63,17 +99,45 @@ class AnnouncementBar extends Component {
                   );
                 })}
               </Marquee>
-            )}
+            )} */}
           </Grid>
         </Grid>
         <Dialog
           open={this.state.isOpen}
           fullWidth
-          maxWidth="sm"
+          maxWidth="md"
           closeAfterTransition
-          onClose={this.state.handleClose}
         >
-          <DialogContent>HELLO</DialogContent>
+          <DialogTitle>
+            <Grid container spacing={0}>
+              <Grid item xs={6}>
+                <Typography style={{ fontWeight: "bolder", fontSize: 20 }}>
+                  Promotion: Free Shipping for FLX Members
+                </Typography>
+              </Grid>
+              <Grid item xs={6}>
+                <CloseIcon
+                  style={{ float: "right", cursor: "pointer" }}
+                  onClick={() => this.setState({ isOpen: false })}
+                />
+              </Grid>
+            </Grid>
+          </DialogTitle>
+          <DialogContent dividers style={{ padding: 50, fontSize: 16 }}>
+            <Typography gutterBottom>
+              Free standard shipping with no minimums for FLX members; some
+              exclusions apply. Non-member`s order value must total $49 or more
+              before services, taxes, and shipping & handling. Savings are
+              automatically applied at checkout. Offer is limited to standard
+              ground delivery within the 48 contiguous United States and APO/FPO
+              addresses. Entire order must ship to a single address. Excludes
+              drop ships and any items with fixed shipping rates. Does not apply
+              to prior purchases or open orders. Customer is responsible for
+              shipping costs on returned merchandise. Promotion may be modified
+              or terminated at any time. Valid online at footlocker.com, by
+              phone, or by mail. Certain restrictions and exclusions apply.
+            </Typography>
+          </DialogContent>
         </Dialog>
       </React.Fragment>
     );
--- src/components/Dashboard/Contents/appBar.js
+++ src/components/Dashboard/Contents/appBar.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
-import { AppBar, Typography, Toolbar, Container } from "@mui/material";
+import { AppBar, Typography, Toolbar, Container, Grid } from "@mui/material";
+import Logo from "../../../static/images/homepagelogo.jpg";
 import "./announcement.css";
 
 class AnnouncementBar extends Component {
@@ -28,28 +29,38 @@ class AnnouncementBar extends Component {
   render() {
     return (
       <React.Fragment>
-        <AppBar position="static" sx={{ bgcolor: "black" }}>
-          <Container>
+        <AppBar position="static" sx={{ bgcolor: "black", width: 1 }}>
+          <Container style={{ marginLeft: 100, marginRight: 0 }}>
             <Toolbar disableGutters>
-              <div style={{ backgroundColor: "white" }}>
-                <Typography
-                  variant="h6"
-                  noWrap
-                  component="a"
-                  href="/"
-                  sx={{
-                    mr: 2,
-                    display: { xs: "none", md: "flex" },
-                    fontFamily: "monospace",
-                    fontWeight: 700,
-                    letterSpacing: ".3rem",
-                    color: "black",
-                    textDecoration: "none",
-                  }}
-                >
-                  FOOT LOCKER
-                </Typography>
-              </div>
+              <Grid container spacing={0}>
+                <Grid item xs={3}>
+                  <div style={{ backgroundColor: "black" }}>
+                    <img src={Logo} alt="Logo" />
+                    <Typography
+                      variant="h4"
+                      noWrap
+                      component="a"
+                      href="/"
+                      sx={{
+                        mr: 2,
+                        display: { xs: "none", md: "flex" },
+                        fontWeight: "bolder",
+                        letterSpacing: ".3rem",
+                        color: "white",
+                        textDecoration: "none",
+                      }}
+                    >
+                      BOGSILOG
+                    </Typography>
+                  </div>
+                </Grid>
+                <Grid item xs={5}>
+                  {/* Menu */}
+                </Grid>
+                <Grid item xs={4}>
+                  {/* Search */}
+                </Grid>
+              </Grid>
             </Toolbar>
           </Container>
         </AppBar>
